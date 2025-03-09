29912a64201321c32bfcf6c907c1986607d6b74a
zhfrann <muhzhafranilham13@gmail.com>
2025-03-09 16:52:14 +0700

    menambahkan implementasi dengan table-driven

---
+++ tp-modul4/tpmodul4_103022330008/KodePos.cs
@@ -0,0 +1,25 @@
+﻿public class KodePos
+{
+    private string[] kelurahan =
+    {
+        "Batununggal", "Kujangsari", "Mengger", "Wates", "Cijagra",
+        "Jatisari", "Margasari", "Sekejati", "Kebonwaru", "Maleer", "Samoja"
+    };
+
+    private int[] kodePosList =
+    {
+        40266, 40287, 40267, 40256, 40287,
+        40286, 40286, 40286, 40272, 40274, 40273
+    };
+
+    public int? GetKodePos(String kelurahan) {
+        for (int i = 0; i < this.kelurahan.Length; i++)
+        {
+            if (this.kelurahan[i] == kelurahan)
+            {
+                return this.kodePosList[i];
+            }
+        }
+        return null;
+    }
+}
